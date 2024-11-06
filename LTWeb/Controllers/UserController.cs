da2d977b8192195ec41b667bc5bab49d9b3164e8
MiaNgin <lmn262005@gmail.com>
2024-11-06 22:25:31 +0700

    Add project files.

---
+++ LTWeb/Controllers/UserController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LTWeb.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Welcome()
+        {
+            return View();
+        }
+
+        public ActionResult Welcome2()
+        {
+            return View();
+        }
+
+        public ActionResult Welcome3()
+        {
+            return View();
+        }
+    }
+}
---
+++ LTWeb/Scripts/MyScript/UserScript.js
@@ -0,0 +1,5 @@
+﻿funtion HomNay()
+{
+    var Date = new Date();
+    document.write("Today is " + date.getDay() + " month " + date.getMonth() + " year " + date.getFullYear());
+}
